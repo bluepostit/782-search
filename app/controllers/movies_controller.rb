9c2aaa8b57d32dc379966e80d52ed94bab728e82
Yair <32864499+bluepostit@users.noreply.github.com>
2022-02-24 10:03:50 +0100

    add pg search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,16 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      query = <<-SQL
-      title @@ :search_term
-      OR synopsis @@ :search_term
-      OR directors.first_name @@ :search_term
-      OR directors.last_name @@ :search_term
-      SQL
-      @movies = Movie.joins(:director).where(query, search_term: "%#{params[:query]}%")
+      # postgresql full-text search:
+      # query = <<-SQL
+      # title @@ :search_term
+      # OR synopsis @@ :search_term
+      # OR directors.first_name @@ :search_term
+      # OR directors.last_name @@ :search_term
+      # SQL
+      # @movies = Movie.joins(:director).where(query, search_term: "%#{params[:query]}%")
+
+      @movies = Movie.global_search(params[:query])
     else
       @movies = Movie.all
     end
